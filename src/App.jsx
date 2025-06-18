b36f90766373cca935e9c5dd722bb3ba5395f152
thuluu03 <98506834+thuluu03@users.noreply.github.com>
2025-06-17 18:13:49 -0700

    fix: card resets position

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,33 @@ import { Card, data } from './components/Card.jsx'
 
 function App() {
   const [index, setIndex] = useState(0)
+  const [isFlipped, setIsFlipped] = useState(false)
 
   const handlePrev = () => {
     if (index >= 1) {
       setIndex(index-1)
+      setIsFlipped(false)
     }
   }
 
   const handleNext = () => {
     if (index < data.length - 1) {
       setIndex(index+1)
+      setIsFlipped(false)
     }
   }
+
+  const handleFlip = () => setIsFlipped(!isFlipped)
 
   return (
     <>
       <h1>Name that Dog!</h1>
-      <Card index={index}></Card>
+      <Card index={index} isFlipped={isFlipped} onFlip={handleFlip}></Card>
 
       <div className="container">
-        <button className={`button ${ index == 0 ? 'inactive' : ''}`}onClick={handlePrev}>Prev</button>
-        <button className={`button ${ index == data.length-1 ? 'inactive' : ''}` }onClick={handleNext}>Next</button>
+        <button className={`button ${ index == 0 ? 'inactive' : ''}`} onClick={handlePrev}>Prev</button>
+        <p className="cardCounter">{index + 1}/{data.length} cards</p>
+        <button className={`button ${ index == data.length-1 ? 'inactive' : ''}` } onClick={handleNext}>Next</button>
       </div>
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,23 +5,21 @@ import {useState} from 'react'
 const data = JSON.parse(cards)
 
 const Card = (props) => {
-    const [isFlipped, setIsFlipped] = useState(false);
-
-    const handleFlip = () => setIsFlipped(!isFlipped);
+    const { index, isFlipped, onFlip } = props;
 
     return (
-        <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleFlip}>
+        <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={onFlip}>
             <div className="cardInner">
                 <div className="cardFront">
                     <h3>What dog is this?</h3>
                     <div className="imgContainer">
-                         <img className="cardImg" src={data[props.index].img}></img>
+                         <img className="cardImg" src={data[index].img}></img>
                     </div>
                 </div>
             
                 <div className="cardBack">
                     <h3>That's right!</h3>
-                    <p>It's a {data[props.index].answer}</p>
+                    <p>It's a {data[index].answer}</p>
                 </div>
             </div>
         </div>
