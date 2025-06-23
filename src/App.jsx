ce0cc8004e1917bdec6ffc96da465e7678629e41
thuluu03 <98506834+thuluu03@users.noreply.github.com>
2025-06-22 21:58:03 -0700

    feat: text input & reshuffle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { Card, data } from './components/Card.jsx'
-
+import { distance } from "fastest-levenshtein";
 
 function App() {
   const [isFlipped, setIsFlipped] = useState(false)
   const [history, setHistory] = useState([0])
   const [currentPos, setCurrentPos] = useState(0)
+  const [guess, setGuess] = useState('')
+  const [inputError, setInputError] = useState(false)
 
   const getRandomIndex = () => {
     const allIndices = data.map((_, i) => i);
@@ -36,17 +38,90 @@ function App() {
   }
 
   const handleFlip = () => setIsFlipped(!isFlipped)
+  
+  const handleInput = (e) => {
+    setGuess(e.target.value)
+    setInputError(false);
+  }
+
+  const normalizeString = (s) => {
+    return s
+    .toLowerCase()
+    .replace(/[^\w\s]|_/g, "")
+    .replace(/\s+/g, " ")     
+    .trim();
+  }
+
+  const isValidGuess = (guess, answer) => {
+    const normGuess = normalizeString(guess)
+    const normAnswer = normalizeString(answer)
+    
+    const answerWords = normAnswer.split(" ");
+    return answerWords.some(word => distance(normGuess, word) <= 2);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent page reset
+    // check if valued entered is correct
+    const answer = data[history[currentPos]].answer
+
+    if (isValidGuess(guess, answer)) {
+      setIsFlipped(true);
+      setGuess('')
+    } else {
+      setInputError(true);
+    }
+  }
+
+  const handleShuffle = () => {
+    // set an alert
+
+    // randomly choose first card
+    const allIndices = data.map((_, i) => i);
+    setHistory([allIndices[Math.floor(Math.random() * allIndices.length)]])
+    setCurrentPos(0)
+  }
 
   return (
     <div className='appContainer'>
       <h1>Name that Dog!</h1>
-      <p>Test your knowledge of dog breeks by naming each dog on the flashcards!</p>
+      <p>Test your knowledge of dog breeds by naming each dog on the flashcards!</p>
       <p> Click the card to flip over when you've made your guess.</p>
-      <Card index={history[currentPos]} isFlipped={isFlipped} onFlip={handleFlip}></Card>
+      <Card 
+        index={history[currentPos]} 
+        isFlipped={isFlipped} 
+        onFlip={handleFlip}>
+      </Card>
+      <div className='inputContainer'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input 
+              className={`input ${inputError ? 'inputError' : ''}`} 
+              type='text' value={guess} 
+              onChange={handleInput}/>
+          </label>
+          <input type="submit" value="Submit"/>
+        </form> 
+      </div>
       <div className="buttonContainer">
-        <button className={`button ${ currentPos == 0 ? 'inactive' : ''}`} onClick={handlePrev}>Prev</button>
+        <button 
+          className={`button ${ currentPos == 0 ? 'inactive' : ''}`} 
+          onClick={handlePrev}
+          >
+            Prev
+        </button>
         <p className="cardCounter">{currentPos + 1}/{data.length} cards</p>
-        <button className={`button ${ currentPos == data.length-1 ? 'inactive' : ''}` } onClick={handleNext}>Next</button>
+        <button 
+          className={`button ${ currentPos == data.length-1 ? 'inactive' : ''}` } 
+          onClick={handleNext}
+          >
+            Next
+        </button>
+      </div>
+      <div>
+        <button
+          className="shuffleBtn"
+          onClick={handleShuffle}
+        />
       </div>
     </div>
   )
